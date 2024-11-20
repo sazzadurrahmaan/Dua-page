e2bb9686c02af28c211c6f9f3e6cb48dadf2ee90
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-11-21 00:15:31 +0600

    downloaded all necessary images and made the markup of the left bar

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,10 @@
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
+import duaPage from '@/public/assets/dua-logo.svg'
+const inter = Inter({ subsets: ["latin"] });
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,10 +14,176 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <div className="bg-[#f7f8fa]">
+          <div className="grid grid-cols-12 grid-rows-12 gap-4 px-5 pt-5 h-screen">
+            {/* ----start Left-bar--- */}
+            <div className="row-span-full hidden xl:block w-[105px] ">
+              <div
+                id="left-bar"
+                className="w-[100px] flex flex-col gap-y-5 overflow-hidden"
+              >
+                <div className="bg-white rounded-3xl px-4 dark:bg-dark-bg h-[95vh] scrl-left pb-16 border">
+                  <div className="pt-7 mb-10 xl:mb-16 2xl:mb-16 3xl:mb-24 container ">
+                    <Link href="/">
+                      <Image
+                        src={duaPage}
+                        alt="dua-logo"
+                        className="h-13 w-16 mx-auto drop-shadow-[#1fa45b] xl:mb-0 cursor-pointer"
+                        width={20}
+                        height={20}
+                      />
+                    </Link>
+                  </div>
+                  <Link href="/">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/home.svg"
+                            alt="/"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href="/alldua">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/alldua.svg"
+                            alt="/alldua"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href="/memorize">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/memorize.svg"
+                            alt="/memorize"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href="/bookmark">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/bookmark.svg"
+                            alt="/bookmark"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href="/ruqyah">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/ruqyah.svg"
+                            alt="/ruqyah"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href="/dua-info">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/dua-info.svg"
+                            alt="/dua-info"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href="/books">
+                    <div className="justify-center items-center  cursor-pointer my-2 flex flex-row w-full rounded-lg group">
+                      <div className=" w-1 rounded-tl-lg rounded-bl-lg " />
+                      <div className="p-2 flex flex-row justify-center items-center">
+                        <div className=" bg-[#f7f8fa] dark:bg-dark-bg-dark flex p-2 rounded-full items-center h-10 w-10 justify-center">
+                          <Image
+                            src="../public/assets/nav/books.svg"
+                            alt="/books"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                        <h2 className="text-mute-grey-200 dark:text-dark-text style-Kalpurush text-sm text-start group-hover:text-[#1fa45b] font-medium " />
+                      </div>
+                    </div>
+                  </Link>
+                  <div>
+                    <div className="pt-6 xl:pt-16 2xl:pt-16 3xl:pt-24 pb-2 pl-2 text-center">
+                      <Link href="/support">
+                        <button className="w-13 h-13 bg-[#1fa45b] drop-shadow-[#1fa45b] rounded-lg lg:mr-4 text-white text-sm">
+                          <div className="flex justify-center items-center">
+                            <p className="hidden">I Want To Support</p>{" "}
+                            <svg
+                              width={50}
+                              height={50}
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M19.2577 7.39C19.7087 6.925 19.9877 6.282 19.9877 5.572C19.9877 4.862 19.7087 4.219 19.2577 3.754C19.0296 3.51598 18.7558 3.3265 18.4527 3.19693C18.1496 3.06736 17.8234 3.00038 17.4937 3C17.4937 3 16.2497 2.997 14.9997 4.286C13.7497 2.997 12.5057 3 12.5057 3C12.1762 3.00042 11.8501 3.06732 11.547 3.19671C11.2439 3.32609 10.97 3.5153 10.7417 3.753C10.2907 4.219 10.0117 4.861 10.0117 5.571C10.0117 6.281 10.2907 6.925 10.7417 7.389L13.5305 10.409C14.3224 11.2665 15.6772 11.2666 16.4691 10.4092L19.2577 7.39Z"
+                                fill="white"
+                              />
+                              <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M19.226 13.02L16.0478 15.5619C15.6932 15.8455 15.2526 16 14.7986 16H11C10.7239 16 10.5 15.7761 10.5 15.5C10.5 15.2239 10.7239 15 11 15H14.565C14.6554 15 14.7442 14.9754 14.8218 14.9289C14.8993 14.8824 14.9629 14.8157 15.0055 14.736C15.0482 14.6563 15.0685 14.5664 15.0642 14.4761C15.0598 14.3858 15.0311 14.2983 14.981 14.223L14.093 12.891C13.9108 12.6168 13.6636 12.392 13.3735 12.2365C13.0833 12.081 12.7592 11.9998 12.43 12H7C6.73478 12 6.48043 12.1054 6.29289 12.2929C6.26671 12.3191 6.22752 12.3615 6.17673 12.4164C5.86372 12.755 5.1104 13.5698 4.25 14L1.5 14V19H3.75C5 19 6.12493 19.1249 6.5 19.5C6.87507 19.8751 7.46957 20 8 20H15.139C15.5666 20.0001 15.9893 19.9088 16.3787 19.7322C16.7682 19.5556 17.1153 19.2977 17.397 18.976L22.4064 14.0708C22.906 13.5816 22.7114 12.7371 22.048 12.516C21.573 12.3577 21.0658 12.3208 20.5729 12.4089C20.0799 12.4969 19.6169 12.707 19.226 13.02Z"
+                                fill="white"
+                              />
+                            </svg>
+                          </div>
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* ---End Left-bar--- */}
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
