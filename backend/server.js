2808c9a8cc74460b4ee4307d00fbd8be5a08d252
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-11-21 21:59:50 +0600

    Endpoint for fetching all dua

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,14 @@ const db = new sqlite3.Database("./database/dua_main.sqlite");
 
 // Endpoint for fetching all categories
 app.get("/api/categories", (req, res) => {
-  db.all("SELECT * FROM category", (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).json({ error: "Internal Server Error" });
-    } else {
-      res.json(rows);
-    }
-  });
+    db.all("SELECT * FROM category", (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: "Internal Server Error" });
+        } else {
+            res.json(rows);
+        }
+    });
 });
 
 
@@ -23,19 +23,30 @@ app.get("/api/categories", (req, res) => {
 
 app.get("/api/sub_categories", (req, res) => {
     db.all("SELECT * FROM sub_category", (err, rows) => {
-      if (err) {
-        console.error(err.message);
-        res.status(500).json({ error: "Internal Server Error" });
-      } else {
-        res.json(rows);
-      }
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: "Internal Server Error" });
+        } else {
+            res.json(rows);
+        }
     });
-  });
-  
+});
 
+
+
+// Endpoint for fetching all duas
+app.get("/api/duas", (req, res) => {
+    db.all("SELECT * FROM dua", (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: "Internal Server Error" });
+        } else {
+            res.json(rows);
+        }
+    });
+});
 
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-  });
-  
+});
